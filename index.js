e41d60861a808eb6cb9fd43f8c8025a0c29a6289
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-13 17:07:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//Declaring customerName in global scope
+var customerName = "bob";
+//Function to uppercase customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+//Function to set bestCustomer
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+//Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+//leastFavoriteCustomer declared as a constant
+const leastFavoriteCustomer = "maybe bob";
+
+//Function to try change the constant
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new bob";
+}
